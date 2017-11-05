0d06fec0fec0f850c1e59bcea7b341862ee2094f
Justin Davis <jedavis83@gmail.com>
2017-11-05 15:54:51 -0800

    add models

--- spacetime/stweb/models.py
+++ spacetime/stweb/models.py
@@ -3,4 +3,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class TimeStamp(models.Model):
+    stamp = models.DateTimeField()
+    # TODO, interval
+
+
+class Location(models.Model):
+    name = models.TextField()
+
+
+class EventType(models.Model):
+    name = models.TextField()
+    
+
+class Event(models.Model):
+    timestamp = models.ForeignKey(TimeStamp)
+    location = models.ForeignKey(Location)
+    eventtype = models.ForeignKey(EventType)
