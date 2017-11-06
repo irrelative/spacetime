2d10f6bb0c0a98d0546e818a5ebdb2310acd805b
Justin Davis <jedavis83@gmail.com>
2017-11-05 20:59:49 -0800

    basic view

--- spacetime/spacetime/urls.py
+++ spacetime/spacetime/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from stweb import views
+
 urlpatterns = [
+    url(r'^', views.EventListView.as_view(), name='event-list'),
     url(r'^admin/', admin.site.urls),
 ]
--- spacetime/stweb/views.py
+++ spacetime/stweb/views.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Event
 
-# Create your views here.
+class EventListView(ListView):
+
+    model = Event
+
+    def get_context_data(self, **kwargs):
+        data = super(EventListView, self).get_context_data(**kwargs)
+        return data
