436a97b001c82a277c080776d3e5a0ab1b25d764
Justin Davis <jedavis83@gmail.com>
2017-11-05 16:02:19 -0800

    create via admin

--- spacetime/stweb/admin.py
+++ spacetime/stweb/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Event, Location, EventType, TimeStamp
+
 
 # Register your models here.
+admin.site.register(Event)
+admin.site.register(EventType)
+admin.site.register(Location)
+admin.site.register(TimeStamp)
